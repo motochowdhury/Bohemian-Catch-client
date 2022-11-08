8930c297dd9d8ab99c2cf7e3f53f7fd3d795296f
Moto Chowdhury <motochowdhury@gmail.com>
2022-11-08 21:36:33 +0600

    Review section done

--- src/Pages/ServiceDetails.jsx
+++ src/Pages/ServiceDetails.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const ServiceDetails = () => {
-  const { img, price, serviceName, desc } = useLoaderData();
+  const { img, price, serviceName, desc, _id } = useLoaderData();
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/reviews?id=${_id}`)
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+
   return (
     <div className="w-full bg-[#FCFBF3] py-10">
       <div className="max-w-6xl mx-auto gap-8 grid grid-cols-12">
@@ -20,7 +27,46 @@ const ServiceDetails = () => {
             <p className="font-medium font-[Roboto Slab]">{desc}</p>
           </div>
         </div>
-        <div className="col-span-7 w-full h-96 bg-[#D4FAEC] rounded-xl"></div>
+        <div className="col-span-7 w-full bg-[#D4FAEC] rounded-xl">
+          <div>
+            <h4 className="font-bold font-[poppins] text-2xl text-center mt-3">
+              Reviews
+            </h4>
+            {reviews.length === 0 ? (
+              <div className="flex mt-20 items-center justify-center text-2xl font-bold font-[poppins]">
+                <h1>No Reviews Added yet</h1>
+              </div>
+            ) : (
+              reviews.map((review) => (
+                <div className="flex bg-white m-4 p-3 rounded-xl shadow">
+                  <div className="flex items-center">
+                    <div className="w-16 h-16">
+                      <img
+                        src={
+                          review.img
+                            ? review.img
+                            : "https://t3.ftcdn.net/jpg/02/18/21/86/360_F_218218632_jF6XAkcrlBjv1mAg9Ow0UBMLBaJrhygH.jpg"
+                        }
+                        className="w-16 h-16 rounded-full ring-4 ring-[#04DA8D]"
+                        alt=""
+                      />
+                    </div>
+                    <div className="border-r-2 px-4  border-black">
+                      <h4 className="font-semibold font-[poppins]">
+                        {review.name}
+                      </h4>
+                    </div>
+                  </div>
+                  <div className="w-[90%] px-4 flex items-center">
+                    <p className="font-medium font-[poppins] text-sm">
+                      {review.review}
+                    </p>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
