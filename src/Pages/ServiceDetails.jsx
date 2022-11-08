06e80023606a795e8702c43b9d9d92e53160c3e5
Moto Chowdhury <motochowdhury@gmail.com>
2022-11-08 18:45:45 +0600

    details section done

--- src/Pages/ServiceDetails.jsx
+++ src/Pages/ServiceDetails.jsx
@@ -3,7 +3,27 @@ import { useLoaderData } from "react-router-dom";
 
 const ServiceDetails = () => {
   const { img, price, serviceName, desc } = useLoaderData();
-  return <></>;
+  return (
+    <div className="w-full bg-[#FCFBF3] py-10">
+      <div className="max-w-6xl mx-auto gap-8 grid grid-cols-12">
+        <div className="col-span-5 bg-[#D4FAEC] h-96 rounded-xl">
+          <div className="p-4 flex items-center">
+            <img src={img} alt="" className="m-4 w-1/2 rounded-xl" />
+            <div>
+              <h2 className="text-2xl font-[poppins] font-semibold text-black">
+                {serviceName}
+              </h2>
+              <p className="font-[poppins] font-semibold mt-3 text-[#04DA8D]">{`price: $${price}`}</p>
+            </div>
+          </div>
+          <div className="m-5">
+            <p className="font-medium font-[Roboto Slab]">{desc}</p>
+          </div>
+        </div>
+        <div className="col-span-7 w-full h-96 bg-[#D4FAEC] rounded-xl"></div>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceDetails;
