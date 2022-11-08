7e1c5548e6ffc3d570d4cc39c7ed7729567ae715
Moto Chowdhury <motochowdhury@gmail.com>
2022-11-09 00:24:34 +0600

    Animation added with react reveal

--- src/Pages/ServiceDetails.jsx
+++ src/Pages/ServiceDetails.jsx
@@ -1,9 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, Navigate, useLoaderData, useLocation } from "react-router-dom";
+import { AuthContext } from "../Contexts/AuthProvider";
+import Fade from "react-reveal/Fade";
+import Reveal from "react-reveal/Reveal";
 
 const ServiceDetails = () => {
   const { img, price, serviceName, desc, _id } = useLoaderData();
+  const { user } = useContext(AuthContext);
   const [reviews, setReviews] = useState([]);
+  const [open, setOpen] = useState(false);
   useEffect(() => {
     fetch(`http://localhost:5000/reviews?id=${_id}`)
       .then((res) => res.json())
@@ -12,64 +17,96 @@ const ServiceDetails = () => {
 
   return (
     <div className="w-full bg-[#FCFBF3] py-10">
-      <div className="max-w-6xl mx-auto gap-8 grid grid-cols-12">
-        <div className="col-span-5 bg-[#D4FAEC] h-96 rounded-xl">
-          <div className="p-4 flex items-center">
-            <img src={img} alt="" className="m-4 w-1/2 rounded-xl" />
-            <div>
-              <h2 className="text-2xl font-[poppins] font-semibold text-black">
-                {serviceName}
-              </h2>
-              <p className="font-[poppins] font-semibold mt-3 text-[#04DA8D]">{`price: $${price}`}</p>
+      <Fade bottom>
+        <div className="max-w-6xl mx-auto">
+          <div className="gap-8 grid grid-cols-12">
+            <div className="col-span-5 bg-[#D4FAEC] h-96 rounded-xl">
+              <div className="p-4 flex items-center">
+                <img src={img} alt="" className="m-4 w-1/2 rounded-xl" />
+                <div>
+                  <h2 className="text-2xl font-[poppins] font-semibold text-black">
+                    {serviceName}
+                  </h2>
+                  <p className="font-[poppins] font-semibold mt-3 text-[#04DA8D]">{`price: $${price}`}</p>
+                </div>
+              </div>
+              <div className="m-5">
+                <p className="font-medium font-[Roboto Slab]">{desc}</p>
+              </div>
+            </div>
+            <div className="col-span-7 w-full bg-[#D4FAEC] rounded-xl">
+              <div>
+                <h4 className="font-bold font-[poppins] text-2xl text-center mt-3">
+                  Reviews
+                </h4>
+                {reviews.length === 0 ? (
+                  <div className="flex mt-20 items-center justify-center text-2xl font-bold font-[poppins]">
+                    <h1>No Reviews Added yet</h1>
+                  </div>
+                ) : (
+                  reviews.map((review) => (
+                    <Reveal effect="fadeInUp">
+                      <div
+                        key={review._id}
+                        className="flex bg-white m-4 p-3 rounded-xl shadow">
+                        <div className="flex items-center">
+                          <div className="w-16 h-16">
+                            <img
+                              src={
+                                review.img
+                                  ? review.img
+                                  : "https://t3.ftcdn.net/jpg/02/18/21/86/360_F_218218632_jF6XAkcrlBjv1mAg9Ow0UBMLBaJrhygH.jpg"
+                              }
+                              className="w-16 h-16 rounded-full ring-4 ring-[#04DA8D]"
+                              alt=""
+                            />
+                          </div>
+                          <div className="border-r-2 px-4  border-black">
+                            <h4 className="font-semibold font-[poppins]">
+                              {review.name}
+                            </h4>
+                          </div>
+                        </div>
+                        <div className="w-[90%] px-4 flex items-center">
+                          <p className="font-medium font-[poppins] text-sm">
+                            {review.review}
+                          </p>
+                        </div>
+                      </div>
+                    </Reveal>
+                  ))
+                )}
+                <div className="mt-10">
+                  <div className="flex justify-center">
+                    <button
+                      onClick={() => setOpen(!open)}
+                      className="btn bg-black px-5 py-3 rounded-full text-white text-sm">
+                      AddReview
+                    </button>
+                  </div>
+                  {user ? (
+                    <div className={`${open ? "block" : "hidden"}`}>
+                      <div>
+                        <form></form>
+                      </div>
+                    </div>
+                  ) : (
+                    <div
+                      className={`${
+                        open ? "block" : "hidden"
+                      } flex justify-center pb-10`}>
+                      <h1>You Have to login</h1>
+                      <Link to="/login" className="font-medium font-[poppins]">
+                        Login
+                      </Link>
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
           </div>
-          <div className="m-5">
-            <p className="font-medium font-[Roboto Slab]">{desc}</p>
-          </div>
         </div>
-        <div className="col-span-7 w-full bg-[#D4FAEC] rounded-xl">
-          <div>
-            <h4 className="font-bold font-[poppins] text-2xl text-center mt-3">
-              Reviews
-            </h4>
-            {reviews.length === 0 ? (
-              <div className="flex mt-20 items-center justify-center text-2xl font-bold font-[poppins]">
-                <h1>No Reviews Added yet</h1>
-              </div>
-            ) : (
-              reviews.map((review) => (
-                <div
-                  key={review._id}
-                  className="flex bg-white m-4 p-3 rounded-xl shadow">
-                  <div className="flex items-center">
-                    <div className="w-16 h-16">
-                      <img
-                        src={
-                          review.img
-                            ? review.img
-                            : "https://t3.ftcdn.net/jpg/02/18/21/86/360_F_218218632_jF6XAkcrlBjv1mAg9Ow0UBMLBaJrhygH.jpg"
-                        }
-                        className="w-16 h-16 rounded-full ring-4 ring-[#04DA8D]"
-                        alt=""
-                      />
-                    </div>
-                    <div className="border-r-2 px-4  border-black">
-                      <h4 className="font-semibold font-[poppins]">
-                        {review.name}
-                      </h4>
-                    </div>
-                  </div>
-                  <div className="w-[90%] px-4 flex items-center">
-                    <p className="font-medium font-[poppins] text-sm">
-                      {review.review}
-                    </p>
-                  </div>
-                </div>
-              ))
-            )}
-          </div>
-        </div>
-      </div>
+      </Fade>
     </div>
   );
 };
